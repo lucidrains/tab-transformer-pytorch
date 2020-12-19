20e32522950422fcd95d885a5545d9c577435468
Phil Wang <lucidrains@gmail.com>
2020-12-19 15:05:47 -0800

    0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tab-transformer-pytorch',
   packages = find_packages(),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'Tab Transformer - Pytorch',
   author = 'Phil Wang',
