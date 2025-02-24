c1b9aa9c28f530d22ee95410842685525646bf64
lucidrains <lucidrains@gmail.com>
2025-02-23 16:11:04 -0800

    add hyper connect dep

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tab-transformer-pytorch',
   packages = find_packages(),
-  version = '0.4.1',
+  version = '0.4.2',
   license='MIT',
   description = 'Tab Transformer - Pytorch',
   long_description_content_type = 'text/markdown',
@@ -18,6 +18,7 @@ setup(
   ],
   install_requires=[
     'einops>=0.8',
+    'hyper-connections>=0.1.15',
     'torch>=2.3'
   ],
   classifiers=[
